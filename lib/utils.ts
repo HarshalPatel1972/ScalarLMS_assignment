0da7455c89f64386a452d07faa78d5e3dca42c27
Harshal Patel <hp842484@gmail.com>
2025-08-06 10:00:00 +0530

    refactor: centralize S3 image URL construction logic in utils for SSR support

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function constructUrl(key: string): string {
+  return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME_IMAGES}.fly.storage.tigris.dev/${key}`;
+}
+
