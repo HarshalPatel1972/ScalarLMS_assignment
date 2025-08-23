dd016faa0813d8d50ba8f2bae690064c28be0969
HarshalPatel1972 <hp842484@gmail.com>
2025-08-24 01:35:18 +0530

    feat(admin-course-card): improve course action menu and UI

---
+++ app/data/admin/admin-get-courses.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/db";
+import { requireAdmin } from "./require-admin";
+import { admin } from "better-auth/plugins";
+
+export async function adminGetCourses() {
+  await requireAdmin();
+
+  const data = await prisma.course.findMany({
+    orderBy: {
+      createdAt: "desc", //newest courses first
+    },
+    select: {
+      id: true,
+      title: true,
+      smallDescription: true,
+      duration: true,
+      level: true,
+      status: true,
+      price: true,
+      fileKey: true,
+      slug: true,
+    },
+  });
+
+  return data;
+}
+
+export type AdminCourseType = Awaited<ReturnType<typeof adminGetCourses>>[0];
---
+++ hooks/use-construct-url.ts
@@ -0,0 +1,7 @@
+import { env } from "@/lib/env";
+
+export function useConstructUrl(key: string): string {
+  return `https://${env.NEXT_PUBLIC_S3_BUCKET_NAME_IMAGES}.fly.storage.tigris.dev/${key}`;
+}
+
+//return type od above function is string
