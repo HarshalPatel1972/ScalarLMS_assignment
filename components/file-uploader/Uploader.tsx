86d63ca4f72d06347b6955b8347473cd4d4bf6fb
HarshalPatel1972 <hp842484@gmail.com>
2025-08-23 07:15:20 +0530

    Integrate Tigris S3 client for file uploads; updated Uploader.tsx to use presigned URLs and added S3Client.ts configuration

--- components/file-uploader/Uploader.tsx
+++ components/file-uploader/Uploader.tsx
@@ -1,15 +1,145 @@
 "use client";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { FileRejection, useDropzone } from "react-dropzone";
 import { Card, CardContent } from "../ui/card";
 import { cn } from "@/lib/utils";
 import { RenderEmptyState, RenderErrorState } from "./RenderState";
 import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
+import { Preahvihear } from "next/font/google";
+
+interface UploaderState {
+  id: string | null;
+  file: File | null;
+  uploading: boolean;
+  progress: number;
+  key?: string;
+  isDeleting: boolean;
+  error: boolean;
+  objectUrl?: string;
+  fileType: "image" | "video";
+}
 
 export function Uploader() {
+  //to store
+  const [fileState, setFileState] = useState<UploaderState>({
+    error: false,
+    file: null,
+    id: null,
+    uploading: false,
+    progress: 0,
+    isDeleting: false,
+    fileType: "image",
+  });
+
+  //upload a file for real
+  async function uploadFile(file: File) {
+    setFileState((prev) => ({
+      ...prev,
+      uploading: true,
+      progress: 0,
+    }));
+
+    try {
+      //1. get presigned url
+      const presignedResponse = await fetch("/api/s3/upload", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fileName: file.name,
+          contentType: file.type,
+          size: file.size,
+          isImage: true,
+        }),
+      });
+
+      if (!presignedResponse.ok) {
+        toast.error("Failed to get presigned URL");
+        setFileState((prev) => ({
+          ...prev,
+          uploading: false,
+          progress: 0,
+          error: true,
+        }));
+
+        return;
+      }
+
+      //went successfull
+      const { presignedUrl, key } = await presignedResponse.json();
+
+      await new Promise<void>((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        //progress event
+        xhr.upload.onprogress = (event) => {
+          //periodically runs
+          if (event.lengthComputable) {
+            const percentageCompleted = (event.loaded / event.total) * 100;
+            setFileState((prev) => ({
+              ...prev,
+
+              progress: Math.round(percentageCompleted),
+            }));
+          }
+        };
+        //onload event
+
+        xhr.onload = () => {
+          if (xhr.status == 200 || xhr.status === 204) {
+            setFileState((prev) => ({
+              ...prev,
+              progress: 100,
+              uploading: false,
+              key: key,
+            }));
+
+            toast.success("File Uploaded Successfully");
+
+            resolve();
+          } else {
+            reject(new Error("Upload failed..."));
+          }
+        };
+        xhr.onerror = () => {
+          reject(new Error("Upload failed"));
+        };
+
+        xhr.open("PUT", presignedUrl);
+        xhr.setRequestHeader("Content-Type", file.type);
+        xhr.send(file);
+      });
+    } catch {
+      toast.error("Something went wrong");
+      setFileState((prev) => ({
+        ...prev,
+        uploading: false,
+        progress: 0,
+        error: true,
+      }));
+    }
+  }
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Do something with the files
-    console.log(acceptedFiles);
+    // console.log(acceptedFiles);
+
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
+
+      setFileState({
+        file: file,
+        uploading: false,
+        progress: 0,
+        objectUrl: URL.createObjectURL(file),
+        error: false,
+        id: uuidv4(),
+        isDeleting: false,
+        fileType: "image",
+      });
+
+      uploadFile(file); //after xhr creation we can put this thing function here
+    }
   }, []);
 
   function rejectedFiles(fileRejection: FileRejection[]) {
@@ -30,6 +160,23 @@ export function Uploader() {
         toast.error("Too many files selected, max is 1");
       }
     }
+  }
+
+  //let's decide which state of files upload we are dealing with, let's write a function for it
+  function renderContent() {
+    if (fileState.uploading) {
+      return <h1>uploading...</h1>;
+    }
+
+    if (fileState.error) {
+      return <RenderErrorState />;
+    }
+
+    if (fileState.objectUrl) {
+      return <h1>uploaded file</h1>;
+    }
+
+    return <RenderEmptyState isDragActive={isDragActive} />;
   }
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -55,9 +202,13 @@ export function Uploader() {
       <input {...getInputProps()} />{" "}
       {/* to get the buttons uploading the files working */}
       <CardContent className="flex items-center justify-center h-full w-full p-4">
-        <RenderEmptyState isDragActive={isDragActive} />
+        {/* <RenderEmptyState isDragActive={isDragActive} /> */}
 
         {/* <RenderErrorState /> */}
+
+        {/* INSTEAD OF HARDCODING STATES NOW WE CAN USE THE RENDER STATE FUNCTION */}
+
+        {renderContent()}
       </CardContent>
     </Card>
   );
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,3 +1,4 @@
+// import "server-only";
 import { createAuthClient } from "better-auth/react";
 import { emailOTPClient } from "better-auth/client/plugins";
 
