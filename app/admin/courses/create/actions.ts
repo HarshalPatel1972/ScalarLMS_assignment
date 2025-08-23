52f54dd5d724e402670cb474e4c549741e1d6327
HarshalPatel1972 <hp842484@gmail.com>
2025-08-23 21:59:33 +0530

    feat(course): add CreateCourse server action with validation, security, and DB logic

--- app/admin/courses/create/actions.ts
+++ app/admin/courses/create/actions.ts
@@ -1,5 +1,87 @@
 "use server";
 
-import { CourseSchemaType } from "@/lib/zodSchemas";
+import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { prisma } from "@/lib/db";
+import { ApiResponse } from "@/lib/types";
+import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import { request } from "@arcjet/next";
 
-export async function CreateCourse(data: CourseSchemaType) {}
+//lets setup arcjet
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [], //no bot is allowed to interact
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
+
+export async function CreateCourse(
+  values: CourseSchemaType
+): Promise<ApiResponse> {
+  const session = await requireAdmin();
+  try {
+    // const session = await auth.api.getSession({
+    //   headers: await headers(),
+    // });
+
+    // Access request data that Arcjet needs when you call `protect()` similarly
+    // to `await headers()` and `await cookies()` in `next/headers`
+    const req = await request();
+
+    //arcjet protection
+    const decision = await aj.protect(req, {
+      fingerprint: session?.user.id as string,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been blocked due to rate limiting",
+        };
+      } else {
+        return {
+          status: "error",
+          message: "You are a bot! if this is a mistake contact our support",
+        };
+      }
+    }
+
+    const validation = courseSchema.safeParse(values);
+
+    //if failed
+    if (!validation.success) {
+      return {
+        status: "error",
+        message: "Invalid Form Data",
+      };
+    }
+
+    const data = await prisma.course.create({
+      data: {
+        ...validation.data,
+        // userId: "xyz",
+        userId: session.user.id as string,
+      },
+    });
+
+    return {
+      status: "success",
+      message: "Course created successfully",
+    };
+  } catch {
+    // console.log(error);
+    return {
+      status: "error",
+      message: "Failed to create course",
+    };
+  }
+}
---
+++ app/data/admin/require-admin.ts
@@ -0,0 +1,22 @@
+import "server-only";
+
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+export async function requireAdmin() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/login");
+  }
+
+  if (session.user.role !== "admin") {
+    return redirect("/not-admin");
+  }
+
+  //user is valid and admin
+  return session;
+}
